6d727c6c7fecd7fae92e7927834b835fc7db1f46
Jaroslav Havlin <380698+jhavlin@users.noreply.github.com>
2020-03-28 09:07:10 +0100

    Prepare basic layout and background

--- elm/src/CustomEvents.elm
+++ elm/src/CustomEvents.elm
@@ -1,18 +1,24 @@
-module CustomEvents exposing (onMouseDownWithButton, onMouseUpWithButton, onMouseEnterWithButtons)
+module CustomEvents exposing (onMouseDownWithButton, onMouseEnterWithButtons, onMouseUpWithButton)
 
 import Html
 import Html.Events exposing (on)
-import Json.Decode exposing (Decoder, map, int, field)
+import Json.Decode exposing (Decoder, field, int, map)
+
 
 withButtonDecoder : (Int -> msg) -> Decoder msg
-withButtonDecoder message = map message (field "button" int)
+withButtonDecoder message =
+    map message (field "button" int)
+
 
 withButtonsDecoder : (Int -> msg) -> Decoder msg
-withButtonsDecoder message = map message (field "buttons" int)
+withButtonsDecoder message =
+    map message (field "buttons" int)
+
 
 onMouseDownWithButton : (Int -> msg) -> Html.Attribute msg
 onMouseDownWithButton message =
     on "mousedown" <| withButtonDecoder message
+
 
 onMouseUpWithButton : (Int -> msg) -> Html.Attribute msg
 onMouseUpWithButton message =
--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -4,11 +4,11 @@ import Array exposing (Array)
 import Browser
 import Browser.Navigation as Nav
 import CustomEvents exposing (onMouseDownWithButton, onMouseEnterWithButtons)
-import Html exposing (Html, div, h1, text, button, input)
-import Html.Attributes exposing (attribute, class, type_, style)
-import Html.Events exposing (onClick, onMouseDown, onMouseUp, onMouseLeave, onInput)
-import Svg exposing (svg, polygon)
-import Svg.Attributes as SAttr exposing (stroke, fill, viewBox, width, points, strokeWidth)
+import Html exposing (Html, button, div, h1, input, text)
+import Html.Attributes exposing (attribute, class, style, type_)
+import Html.Events exposing (onClick, onInput, onMouseDown, onMouseLeave, onMouseUp)
+import Svg exposing (polygon, svg)
+import Svg.Attributes as SAttr exposing (fill, points, stroke, strokeWidth, viewBox, width)
 import Time
 import Url
 
@@ -36,27 +36,38 @@ main =
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
-    , rotation: Int
-    , colors: Array String
-    , autoRotating: Int
-    , currentColor: String
-    , palette: List String
-    , autoDrawing: Bool
-    , pinnedSegment: Maybe Int
+    , rotation : Int
+    , colors : Array String
+    , autoRotating : Int
+    , currentColor : String
+    , palette : List String
+    , autoDrawing : Bool
+    , pinnedSegment : Maybe Int
+    , viewMode : ViewMode
     }
 
 
-type alias Point = (Int, Int)
+type alias Point =
+    ( Int, Int )
+
+
+type ViewMode
+    = Info
+    | Edit
+    | Show
+    | List
+    | Share
+
 
 type alias Area =
-    { topLeft: Point
-    , topRight: Point
-    , bottomLeft: Point
-    , bottomRight: Point
+    { topLeft : Point
+    , topRight : Point
+    , bottomLeft : Point
+    , bottomRight : Point
     }
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd msg )
 init _ url key =
     let
         model =
@@ -66,16 +77,19 @@ init _ url key =
             , colors = initColorsArray
             , autoRotating = 0
             , currentColor = "#000000"
-            , palette = ["#53b9e9", "#fd6617", "#dd5875", "#8a75ad", "#fffc3f", "#ffffff", "#000000"]
+            , palette = [ "#53b9e9", "#fd6617", "#dd5875", "#8a75ad", "#fffc3f", "#ffffff", "#000000" ]
             , autoDrawing = False
             , pinnedSegment = Nothing
+            , viewMode = Edit
             }
     in
     ( model, Cmd.none )
 
 
-initColorsArray: Array String
-initColorsArray = Array.repeat (layersCount * verticalSegments) ""
+initColorsArray : Array String
+initColorsArray =
+    Array.repeat (layersCount * verticalSegments) ""
+
 
 
 -- UPDATE
@@ -97,7 +111,8 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp -> ( model, Cmd.none )
+        NoOp ->
+            ( model, Cmd.none )
 
         LinkClicked urlRequest ->
             case urlRequest of
@@ -117,12 +132,13 @@ update msg model =
                 newPalette =
                     if String.isEmpty color || List.member color model.palette then
                         model.palette
+
                     else
                         color :: List.take (List.length model.palette - 1) model.palette
             in
             ( { model
-              | colors = Array.set ((layer * verticalSegments) + segment) color model.colors
-              , palette = newPalette
+                | colors = Array.set ((layer * verticalSegments) + segment) color model.colors
+                , palette = newPalette
               }
             , Cmd.none
             )
@@ -157,13 +173,16 @@ update msg model =
             , Cmd.none
             )
 
+
+
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     if model.autoRotating /= 0 then
-        Time.every 100.0 <| (\_ -> Rotate model.autoRotating)
+        Time.every 100.0 <| \_ -> Rotate model.autoRotating
+
     else
         Sub.none
 
@@ -171,25 +190,35 @@ subscriptions model =
 
 -- VIEW
 
-eggColor: String
-eggColor = "#efb67f"
+
+eggColor : String
+eggColor =
+    "#efb67f"
 
 
 verticalSegments : Int
-verticalSegments = 64
+verticalSegments =
+    64
 
 
-verticalCoefficients: List Float
+verticalCoefficients : List Float
 verticalCoefficients =
     let
-        n = verticalSegments // 4
-        angle = pi / 2 / toFloat n
-    in
-        List.range -n n |> List.map (\i -> sin <| toFloat i * angle)
+        n =
+            verticalSegments // 4
 
-verticalCoefficientPairs: List (Float, Float)
-verticalCoefficientPairs = List.map2 (\a b -> (a, b)) verticalCoefficients
-    <| Maybe.withDefault [] <| List.tail verticalCoefficients
+        angle =
+            pi / 2 / toFloat n
+    in
+    List.range -n n |> List.map (\i -> sin <| toFloat i * angle)
+
+
+verticalCoefficientPairs : List ( Float, Float )
+verticalCoefficientPairs =
+    List.map2 (\a b -> ( a, b )) verticalCoefficients <|
+        Maybe.withDefault [] <|
+            List.tail verticalCoefficients
+
 
 layerBorders : List ( Int, Int )
 layerBorders =
@@ -224,29 +253,31 @@ layerBorders =
     ]
 
 
-layerBorderPairs : List ( (Int, Int), (Int, Int) )
-layerBorderPairs = List.map2 (\a b -> (a, b)) layerBorders
-    <| Maybe.withDefault [] <| List.tail layerBorders
+layerBorderPairs : List ( ( Int, Int ), ( Int, Int ) )
+layerBorderPairs =
+    List.map2 (\a b -> ( a, b )) layerBorders <|
+        Maybe.withDefault [] <|
+            List.tail layerBorders
 
 
 layersCount : Int
-layersCount = List.length layerBorderPairs
+layersCount =
+    List.length layerBorderPairs
 
 
-areas: List (List Area)
+areas : List (List Area)
 areas =
     let
-        convertBorderPair ( (w1, y1), (w2, y2) ) =
+        convertBorderPair ( ( w1, y1 ), ( w2, y2 ) ) =
             let
-                toArea (c1, c2) =
-                    { topLeft = (round <| c1 * toFloat w1, y1)
-                    , topRight = (round <| c2 * toFloat w1, y1)
-                    , bottomRight = (round <| c2 * toFloat w2, y2)
-                    , bottomLeft = (round <| c1 * toFloat w2, y2)
+                toArea ( c1, c2 ) =
+                    { topLeft = ( round <| c1 * toFloat w1, y1 )
+                    , topRight = ( round <| c2 * toFloat w1, y1 )
+                    , bottomRight = ( round <| c2 * toFloat w2, y2 )
+                    , bottomLeft = ( round <| c1 * toFloat w2, y2 )
                     }
             in
             List.map toArea verticalCoefficientPairs
-
     in
     List.map convertBorderPair layerBorderPairs
 
@@ -254,7 +285,7 @@ areas =
 polygonPoints : List (List String)
 polygonPoints =
     let
-        pointToStr (x, y) =
+        pointToStr ( x, y ) =
             String.fromInt x ++ "," ++ String.fromInt y
 
         areaToPoints { topLeft, topRight, bottomRight, bottomLeft } =
@@ -268,21 +299,29 @@ polygonPoints =
                 , pointToStr bottomLeft
                 ]
 
-        mapAreaList list = List.map areaToPoints list
+        mapAreaList list =
+            List.map areaToPoints list
     in
-        List.map mapAreaList areas
+    List.map mapAreaList areas
 
 
 picture : Model -> Html Msg
 picture model =
     let
-        areaToShape layerIndex segmentIndex polygonPointsStr  =
+        areaToShape layerIndex segmentIndex polygonPointsStr =
             let
-                visibleSegment = remainderBy verticalSegments <| segmentIndex + verticalSegments + model.rotation
-                fillStr = Array.get ((layerIndex * verticalSegments) + visibleSegment) model.colors |> Maybe.withDefault ""
-                color = if String.isEmpty fillStr
-                    then eggColor
-                    else fillStr
+                visibleSegment =
+                    remainderBy verticalSegments <| segmentIndex + verticalSegments + model.rotation
+
+                fillStr =
+                    Array.get ((layerIndex * verticalSegments) + visibleSegment) model.colors |> Maybe.withDefault ""
+
+                color =
+                    if String.isEmpty fillStr then
+                        eggColor
+
+                    else
+                        fillStr
             in
             polygon
                 [ points polygonPointsStr
@@ -291,28 +330,36 @@ picture model =
                 , strokeWidth "0.1"
                 , SAttr.class "egg-area"
                 , onClick <| SetColor layerIndex visibleSegment model.currentColor
-                , onMouseDownWithButton <| \b ->
-                     if b == 0 then
-                        SetColor layerIndex visibleSegment model.currentColor
-                     else if b == 1 then
-                        PinSegment <| Just visibleSegment
-                     else
-                       NoOp
-                , onMouseEnterWithButtons <| \bs ->
-                    if bs == 1 && model.autoDrawing then
-                        SetColor layerIndex visibleSegment model.currentColor
-                    else if bs == 4 && Maybe.withDefault -1 model.pinnedSegment >= 0 then
-                        SetRotation <| Maybe.withDefault 0 model.pinnedSegment - segmentIndex
-                    else
-                        NoOp
-                ] []
+                , onMouseDownWithButton <|
+                    \b ->
+                        if b == 0 then
+                            SetColor layerIndex visibleSegment model.currentColor
 
-        layerToShapes layerIndex pointsList = List.indexedMap
-            (\i a ->areaToShape layerIndex i a)
-            pointsList
+                        else if b == 1 then
+                            PinSegment <| Just visibleSegment
 
+                        else
+                            NoOp
+                , onMouseEnterWithButtons <|
+                    \bs ->
+                        if bs == 1 && model.autoDrawing then
+                            SetColor layerIndex visibleSegment model.currentColor
 
-        areaShapes = List.concat <| List.indexedMap layerToShapes polygonPoints
+                        else if bs == 4 && Maybe.withDefault -1 model.pinnedSegment >= 0 then
+                            SetRotation <| Maybe.withDefault 0 model.pinnedSegment - segmentIndex
+
+                        else
+                            NoOp
+                ]
+                []
+
+        layerToShapes layerIndex pointsList =
+            List.indexedMap
+                (\i a -> areaToShape layerIndex i a)
+                pointsList
+
+        areaShapes =
+            List.concat <| List.indexedMap layerToShapes polygonPoints
     in
     svg
         [ width "700"
@@ -324,6 +371,7 @@ picture model =
         ]
         areaShapes
 
+
 palette : Model -> Html Msg
 palette model =
     let
@@ -332,44 +380,94 @@ palette model =
                 [ class "palette-color"
                 , onClick <| SetCurrentColor color
                 , style "background" color
-                ] []
+                ]
+                []
 
-        eraseItem = div
+        eraseItem =
+            div
                 [ class "palette-color"
                 , onClick <| SetCurrentColor ""
                 , style "background" "white"
-                ] [ text "☒" ]
+                ]
+                [ text "☒" ]
 
-        chooseItem = input [ type_ "color", class "palette-select", onInput SetCurrentColor] []
+        chooseItem =
+            input [ type_ "color", class "palette-select", onInput SetCurrentColor ] []
 
-        colorItems = List.map colorToItem model.palette
+        colorItems =
+            List.map colorToItem model.palette
 
-        selected = if String.isEmpty model.currentColor then eggColor else model.currentColor
+        selected =
+            if String.isEmpty model.currentColor then
+                eggColor
+
+            else
+                model.currentColor
     in
-    div [ class "palette", style "background" selected ] ((eraseItem :: colorItems) ++ [chooseItem])
+    div [ class "palette-outer", style "background" selected ]
+        [ div [ class "palette-inner base-width" ] ((eraseItem :: colorItems) ++ [ chooseItem ])
+        ]
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Bezkontaktní Velikonoce"
     , body =
-        [ div [ class "notranslate", attribute "translate" "no" ]
-            [ h1 [ class "site-title" ] [ text "Bezkontaktní Velikonoce" ]
-            , div []
-                [ button
-                    [ onClick <| Rotate -1
-                    , onMouseDown <| SetAutoRotation -1
-                    , onMouseUp <| SetAutoRotation 0
-                    , onMouseLeave <| SetAutoRotation 0] [text "<-"]
-                , button
-                    [ onClick <| Rotate 1
-                    , onMouseDown <| SetAutoRotation 1
-                    , onMouseUp <| SetAutoRotation 0
-                    , onMouseLeave <| SetAutoRotation 0
-                    ] [text "->"]
-                ]
-            , palette model
-            , div [ class "picture-container" ] [picture model]
-            ]
+        [ div [ class "main-area notranslate", attribute "translate" "no" ]
+            [ viewMainArea model ]
+        , div [ class "main-menu notranslate", attribute "translate" "no" ]
+            [ viewMainMenu model ]
         ]
     }
+
+
+viewMainArea : Model -> Html Msg
+viewMainArea model =
+    viewEdit model
+
+
+viewMainMenu : Model -> Html Msg
+viewMainMenu model =
+    div [ class "main-menu" ] [ text "Ahoj menu" ]
+
+
+viewEdit : Model -> Html Msg
+viewEdit model =
+    div [ class "view-edit notranslate", attribute "translate" "no" ]
+        [ div [ class "picture-container base-width" ] [ picture model ]
+        , palette model
+
+        -- , div []
+        --     [ button
+        --         [ onClick <| Rotate -1
+        --         , onMouseDown <| SetAutoRotation -1
+        --         , onMouseUp <| SetAutoRotation 0
+        --         , onMouseLeave <| SetAutoRotation 0] [text "<-"]
+        --     , button
+        --         [ onClick <| Rotate 1
+        --         , onMouseDown <| SetAutoRotation 1
+        --         , onMouseUp <| SetAutoRotation 0
+        --         , onMouseLeave <| SetAutoRotation 0
+        --         ] [text "->"]
+        --     ]
+        ]
+
+
+viewShow : Model -> Html msg
+viewShow model =
+    div [] []
+
+
+viewList : Model -> Html msg
+viewList model =
+    div [] []
+
+
+viewInfo : Model -> Html msg
+viewInfo model =
+    div [] []
+
+
+viewShare : Model -> Html msg
+viewShare model =
+    div [] []
